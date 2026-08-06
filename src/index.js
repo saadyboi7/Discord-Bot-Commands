acf056706912ef810de8915ea7b9cfd13c6060a1
saadyboi7 <700015043@gotvdsb.ca>
2026-08-06 16:22:56 +0000

    ok

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ console.log(`✅ Loaded ${client.commands.size} commands`);
 client.once("clientReady", () => {
   console.log(`🤖 Logged in as ${client.user.tag}`);
   console.log(`📋 Prefix: ${PREFIX}`);
-  console.log("SYSTEMS:ONLINE! Ready to help!")
+  console.log("Systems are online! Bot is loaded!")
   client.user.setActivity(`${PREFIX}help for commands`);
 });
 
